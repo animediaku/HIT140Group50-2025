f50b3e3cf8ba893031ceb0a33507a5b11f82e0de
Anh Khoa Pham <Khoa.Phamhbi@gmail.com>
2025-09-10 14:13:30 +0930

    Update and rename food and rat number.py to Number of rats and Food remaining bar graph r.py

--- Number of rats and Food remaining bar graph r.py
+++ Number of rats and Food remaining bar graph r.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-# read csv into a DataFrame
 df = pd.read_csv("dataset2_cleaned.csv")
 
 food = df['food_availability']
@@ -10,10 +8,8 @@ rat_num = df['rat_arrival_number']
 
 plt.bar(rat_num, food)
 
-# Add labels and title
 plt.xlabel('Number of Rats')
 plt.ylabel('food left')
 plt.title('number of rats on the leftover food')
 
-# Display the plot
 plt.show()
