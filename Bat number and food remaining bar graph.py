db8614efad053e62395d341d35f553d47e11c086
Anh Khoa Pham <Khoa.Phamhbi@gmail.com>
2025-09-10 14:12:03 +0930

    Update and rename bats and food.py to Bat number and food remaining bar graph.py

--- Bat number and food remaining bar graph.py
+++ Bat number and food remaining bar graph.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-# read csv into a DataFrame
 df = pd.read_csv("dataset2_cleaned.csv")
 
 food = df['food_availability']
@@ -10,10 +8,8 @@ bat_num = df['bat_landing_number']
 
 plt.bar(bat_num, food)
 
-# Add labels and title
-plt.xlabel('Number of Bats')
-plt.ylabel('food left')
+plt.xlabel('Number of bats')
+plt.ylabel('Food Left')
 plt.title('number of bats on the leftover food')
 
-# Display the plot
 plt.show()
