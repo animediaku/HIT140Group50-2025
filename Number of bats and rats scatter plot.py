fe05fbe7cb564f8f5caaa6eb4f42a0d7d7bd1cdd
Anh Khoa Pham <Khoa.Phamhbi@gmail.com>
2025-09-10 14:14:12 +0930

    Update and rename scatter rat and bat.py to Number of bats and rats scatter plot.py

--- Number of bats and rats scatter plot.py
+++ Number of bats and rats scatter plot.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-# read csv into a DataFrame
 df = pd.read_csv("dataset2_cleaned.csv")
 
 bat_num = df['bat_landing_number']
@@ -10,10 +8,8 @@ rat_num = df['rat_arrival_number']
 
 plt.scatter(rat_num, bat_num)
 
-# Add labels and title
 plt.xlabel('Number of Rats')
 plt.ylabel('number of Bats')
-plt.title('Number of bats compared to number of rats')
+plt.title('Number of bats compared to number of rats correlation')
 
-# Display the plot
 plt.show()
